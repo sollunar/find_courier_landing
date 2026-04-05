f3927aded3a290559e0d99444300da25561ce1c7
sollunar <fuzzriff@gmail.com>
2026-04-05 22:09:49 +0300

    install

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME="shamad/find-courier-site:latest"
+
+echo "Building image: $IMAGE_NAME"
+docker build -t "$IMAGE_NAME" .
+
+echo "Pushing image: $IMAGE_NAME"
+docker push "$IMAGE_NAME"
--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -e
 
+FINDCOURIER_IMAGE="shamad/find-courier-site:latest"
+
 # Create the directory for SSL certs if it doesn't exist
 if [ ! -d /var/lib/findcourier ]; then
-	    echo "Creating /var/lib/findcourier directory..."
-	        mkdir -p /var/lib/findcourier
+        echo "Creating /var/lib/findcourier directory..."
+        mkdir -p /var/lib/findcourier
 fi
 
 echo "Please make sure your SSL certificate and key are in /var/lib/findcourier:"
 echo "  findcourier.crt"
 echo "  findcourier.key"
 
-# Launch Docker Compose with build
-echo "Building and starting the container..."
-docker compose up -d --build
+# Pull the published image first so deploys don't build locally
+echo "Pulling image: $FINDCOURIER_IMAGE"
+docker pull "$FINDCOURIER_IMAGE"
+
+echo "Starting the container..."
+FINDCOURIER_IMAGE="$FINDCOURIER_IMAGE" docker compose up -d
 
 # Show running containers
 docker ps
